fed75479239e9f30a96ed2a2381c252c12d949c4
SanShamor <sanshamor@mail.ru>
2021-06-17 18:59:17 +0300

    Initial Commit

---
+++ ColorizedSwiftUI/ColorizedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSwiftUIApp.swift
+//  ColorizedSwiftUI
+//
+//  Created by Александр on 17.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
